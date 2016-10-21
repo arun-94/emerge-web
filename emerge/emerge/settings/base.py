1eaa84e43fdcbeb420e68bae8a22d6b0f0b44b69
Arun Swaminathan <arunswaminathan94@gmail.com>
2016-10-21 23:42:53 +0530

    Basic Setup Config Added

--- emerge/emerge/settings/base.py
+++ emerge/emerge/settings/base.py
@@ -91,18 +91,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
-    # 'default' : {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': get_env_variable('DATABASE_NAME'),
-    #     'USER': get_env_variable('DATABASE_USER'),
-    #     'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-    #     'HOST': '',
-    #     'PORT': '',
-    # }
 }
 
 
