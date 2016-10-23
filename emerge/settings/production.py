adc3436e7aad0a9fb40acdc778819611891060f7
Arun Swaminathan <arunswaminathan94@gmail.com>
2016-10-23 19:54:08 +0530

    test

--- emerge/settings/production.py
+++ emerge/settings/production.py
@@ -5,21 +5,11 @@ import dj_database_url
 DEBUG = False
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ALLOWED_HOSTS = ['yourappname.herokuapp.com']
+ALLOWED_HOSTS = ['emerge-webapi.herokuapp.com']
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
